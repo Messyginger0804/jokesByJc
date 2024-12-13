1df083636a48a41a5da1cfa88b0ca60a11b4220c
Messyginger0804 <j.c.ashley4363@gmail.com>
2024-12-12 23:58:31 -0600

    finished the extenstion version 2 is ready

--- blog.js
+++ blog.js
@@ -8,17 +8,23 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     const data = await response.json();
-    const { title, url } = data;
-    const image = `https://blogsbyjc.vercel.app/blog-images/jwt.png`
-
-    console.log(data)
-
+    const { title, url, snippet } = data;
+    const mainImage = `https://blogsbyjc.vercel.app/blog-images/jwt.png`; // Main image
+    const fakeImage = `./images/pointing.png`;
 
     blogContainer.innerHTML = `
-      <h3>${title}</h3>
-      <a href="${url}" target="_blank">
-        <img src="${image}" alt="${title}" style="width: 150px; height: auto;" />
-      </a>
+      <div class="blog-layout">
+        <a href="${url}" target="_blank" class="blog-image-wrapper">
+          <img src="${mainImage}" alt="${title}" class="blog-image" />
+        </a>
+        <div class="blog-content">
+          <a href="${url}" target="_blank" class="blog-title">
+            ${title}
+          </a>
+          <p class="blog-description">${snippet}</p>
+        </div>
+      </div>
+      <img src="${fakeImage}" alt="Fake description image" class="fake-image" />
     `;
   } catch (error) {
     console.error("Error fetching blog:", error);
